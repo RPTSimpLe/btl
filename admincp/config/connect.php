36dc3e658fa567916897e31e7d97d309d281655c
RPTSimpLe <simplee2083@gmail.com>
2023-12-02 20:55:59 +0700

    fix sql

--- admincp/config/connect.php
+++ admincp/config/connect.php
@@ -1,11 +1,6 @@
 <?php
-$severname="localhost";
-$username="root";
-$password="";
-$database="fresh_garden";
-$port = 3307; // Chuyển thành kiểu int
 
-$connect = new mysqli("localhost","root", "", "fresh_garden", 3307);
+$connect = new mysqli("localhost","root", "", "fresh_garden");
 if (mysqli_connect_errno()) {
     echo "Lỗi kết nối: " . mysqli_connect_error();
     exit();
