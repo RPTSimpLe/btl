6794c4499f9b076b5b9442907a8f32200bad02eb
RPTSimpLe <simplee2083@gmail.com>
2023-12-02 23:32:48 +0700

    sua cong

--- admincp/config/connect.php
+++ admincp/config/connect.php
@@ -1,6 +1,6 @@
 <?php
 
-$connect = new mysqli("localhost","root", "", "fresh_garden","3307");
+$connect = new mysqli("localhost","root", "", "fresh_garden",);
 if (mysqli_connect_errno()) {
     echo "Lỗi kết nối: " . mysqli_connect_error();
     exit();
